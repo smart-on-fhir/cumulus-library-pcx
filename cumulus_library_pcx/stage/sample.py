938b6b99e0fb0a25d893d4ee0e23d332673206b1
comorbidity <andrew.mcmurry@tch.harvard.edu>
2026-09-16 16:02:31 -0700

    updated sample.py for manifest.py

--- cumulus_library_pcx/stage/sample.py
+++ cumulus_library_pcx/stage/sample.py
@@ -1,33 +1,40 @@
 from pathlib import Path
 from cumulus_library_pcx.tools.settings import ENCOUNTER_REF
-from cumulus_library_pcx.tools import filetool, template, tablespace, manifest
+from cumulus_library_pcx.tools import filetool, template, tablespace
 from cumulus_library_pcx.stage.study_variable import Aspect, list_aspect_names
+from cumulus_library_pcx.tools.manifest import (
+    Action,
+    FileAction,
+    SqlAction,
+    SqlParallelAction,
+    save_actions_toml
+)
 
-########################################################################################################
-# Encounter timing relative to "1st casedef match"
+#-----------------------------------------------------------------------------
+# Sample encounters relative to "1st coded casedef match"
 #
 # pre = before
 # peri = during
 # peri_post = during or after
 # post = after
-##########################################################################################################
+#-----------------------------------------------------------------------------
 TEMPORALITY = ['pre', 'peri', 'peri_post', 'post']
 
-########################################################################################################
-# Step 1: sample_casedef
-##########################################################################################################
-def make_sample(table_name:str|None) -> list[Path]:
+#-----------------------------------------------------------------------------
+# Template Step 1: sample_casedef
+#-----------------------------------------------------------------------------
+def make_template_sample(table_name: str | None) -> list[Path]:
     if not table_name:
         table_name = 'sample_casedef'
     return [template.copy(f"{table_name}.sql", encounter_ref=ENCOUNTER_REF)]
 
-########################################################################################################
-# Step 2: for each Aspect
-##########################################################################################################
-def make_aspect() -> list[Path]:
-    return [_make_aspect(aspect) for aspect in list_aspect_names()]
+#-----------------------------------------------------------------------------
+# Template Step 2: for each Aspect
+#-----------------------------------------------------------------------------
+def make_template_aspect() -> list[Path]:
+    return [_make_template_aspect(aspect) for aspect in list_aspect_names()]
 
-def _make_aspect(aspect: Aspect | str) -> Path:
+def _make_template_aspect(aspect: Aspect | str) -> Path:
     """
     Intended use:
     * Aspect.dx:    sample casedef notes that have an encounter matching 1+ "dx" study variable(s)
@@ -41,31 +48,31 @@ def _make_aspect(aspect: Aspect | str) -> Path:
     :return: path to Athena SQL
     """
     if isinstance(aspect, Aspect):
-        return _make_aspect(aspect.name)
+        return _make_template_aspect(aspect.name)
     content = template.load('sample_casedef_aspect.sql', aspect=aspect, encounter_ref=ENCOUNTER_REF)
     table = tablespace.name_prefix(f'sample_casedef_{aspect}')
     path = filetool.path_athena(f'{table}.sql')
     return filetool.save_athena(path, content)
 
-########################################################################################################
-# Step 3: for each TEMPORALITY
-##########################################################################################################
-def make_temporality() -> list[Path]:
+#-----------------------------------------------------------------------------
+# Template Step 3: for each TEMPORALITY
+#-----------------------------------------------------------------------------
+def make_template_temporality() -> list[Path]:
     template_name = 'sample_casedef_temporality'
-    return [_make_temporality(template_name, temporality) for temporality in TEMPORALITY]
+    return [_make_template_temporality(template_name, temporality) for temporality in TEMPORALITY]
 
-########################################################################################################
-# Step 4: with sample size limits
-##########################################################################################################
-def make_temporality_limit_patient(limit: int = 10) -> list[Path]:
+#-----------------------------------------------------------------------------
+# Template Step 4: with sample size limits
+#-----------------------------------------------------------------------------
+def make_template_temporality_limit_patient(limit: int = 10) -> list[Path]:
     template_name = 'sample_casedef_temporality_limit_patient'
-    return [_make_temporality(template_name, temporality, limit) for temporality in TEMPORALITY]
+    return [_make_template_temporality(template_name, temporality, limit) for temporality in TEMPORALITY]
 
-def make_temporality_limit_note(limit: int = 50) -> list[Path]:
+def make_template_temporality_limit_note(limit: int = 50) -> list[Path]:
     template_name = 'sample_casedef_temporality_limit_note'
-    return [_make_temporality(template_name, temporality, limit) for temporality in TEMPORALITY]
+    return [_make_template_temporality(template_name, temporality, limit) for temporality in TEMPORALITY]
 
-def _make_temporality(template_name:str, temporality:str, limit: int = None) -> Path:
+def _make_template_temporality(template_name:str, temporality:str, limit: int = None) -> Path:
     """
     :param template_name: name of the SQL template to load
     :param temporality: str one of ['pre', 'peri', 'peri_post', 'post']
@@ -87,34 +94,35 @@ def _make_temporality(template_name:str, temporality:str, limit: int = None) -> 
     target_file = filetool.path_athena(f'{target_table}.sql')
     return Path(filetool.write_text(text, target_file))
 
-def make_sample_task() -> list[Path]:
-    task = ['sample_task.sql',
-            'sample_task_aspect.sql',
-            'sample_task_tier.sql']
-    task = [tablespace.name_prefix(t) for t in task]
-    return [filetool.path_athena(t) for t in task]
-
-def make() -> list[Path]:
-    aspect_list = list_aspect_names()
-
-    actions = [
-        manifest.SqlAction(make_sample('sample_casedef'),
-                           'samples from union FHIR DocumentReference + FHIR DiagnosticReport'),
-        manifest.SqlAction(make_sample('sample_casedef_author'),
-                           'calc 1x for each note_ref the note_author_date'),
-        manifest.SqlAction(make_aspect(),
-                           f'sample for aspects {aspect_list}'),
-        manifest.SqlAction(make_temporality(),
-                           f'sample temporality {TEMPORALITY}'),
-        manifest.SqlAction(make_temporality_limit_patient(10),
-                           'sample size limit patients'),
-        manifest.SqlAction(make_temporality_limit_note(50),
-                           'sample size limit notes'),
-        #manifest.SqlAction(make_sample_task(), TODO: not yet implemented
-        #                   'sample task', "build:serial"),
+#-----------------------------------------------------------------------------
+# Actions
+#-----------------------------------------------------------------------------
+def make_actions() -> list[Action]:
+    return [SqlAction(
+                make_template_sample('sample_casedef'),
+                'casedef FHIR DocumentReference + FHIR DiagnosticReport'),
+            SqlAction(
+                make_template_sample('sample_casedef_author'),
+                'calc 1x for each note_ref the note_author_date'),
+            SqlParallelAction(
+                make_template_aspect(),
+                f'sample for aspects {list_aspect_names()}'),
+            SqlParallelAction(
+                make_template_temporality(),
+                f'sample temporality {TEMPORALITY}'),
+            SqlParallelAction(
+                make_template_temporality_limit_patient(10),
+                'sample size limit patients'),
+            SqlParallelAction(
+                make_template_temporality_limit_note(50),
+                'sample size limit notes'),
     ]
 
-    return [manifest.save_actions_toml(actions, 'sample.toml')]
+#-----------------------------------------------------------------------------
+# Make
+#-----------------------------------------------------------------------------
+def make() -> list[Path]:
+    return [save_actions_toml(make_actions(), 'sample.toml')]
 
 if __name__ == '__main__':
     for target in make():
